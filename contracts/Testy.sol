164ea7b36d3859f4d7d63068e6df1c7f679c5f75
Styfan <styfand456@gmail.com>
2020-06-09 14:51:29 +0100

    auto push

--- contracts/Testy.sol
+++ contracts/Testy.sol
@@ -3,55 +3,65 @@ pragma solidity >0.4.99 <0.6.0;
 
 
 contract testy {
-    function initContract() public {
-        accounts[1] = account(
+    uint256[] public pages;
+
+    constructor() public {
+        pages = [
             1,
-            0x1AB71030E9D220588c1FE5DCE9783886eEd7713C,
-            "_userName",
-            "_email",
-            "phoneNumber",
-            "_phAddress",
-            "_password",
-            "logoLink",
-            true
-        );
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            16,
+            17,
+            18,
+            19,
+            20,
+            21,
+            23,
+            24,
+            25,
+            26,
+            27,
+            28,
+            29,
+            30,
+            31,
+            32,
+            34,
+            35,
+            36,
+            37,
+            38,
+            39
+        ];
     }
 
-    struct account {
-        uint256 id;
-        address owner;
-        string userName;
-        string email;
-        string phoneNumber;
-        string phAddress;
-        string password;
-        string logoLink;
-        bool labo;
-    }
+    function getPages(uint256 lot)
+        public
+        view
+        returns (uint256[] memory pagesToReturn)
+    {
+        uint256[] memory finalePages = new uint256[](10);
+        uint256 counter = 0;
+        uint256 from = pages.length - 1 - (lot * 10) + 10;
+        uint256 to = from - 9;
 
-    mapping(uint256 => account) public accounts;
+        for (uint256 i = from; i >= to && i >= 0; i--) {
+            finalePages[counter] = pages[i];
 
-    function editUserInfo(
-        uint256 _id,
-        string memory _userName,
-        string memory _email,
-        string memory phoneNumber,
-        string memory _password,
-        string memory _phAddress,
-        string memory logoLink
-    ) public {
-        account memory userAccount = accounts[_id];
-        //  require(userAccount.owner == msg.sender, "This is not your acccount");
-        accounts[_id] = account(
-            _id,
-            userAccount.owner,
-            _userName,
-            _email,
-            phoneNumber,
-            _phAddress,
-            _password,
-            logoLink,
-            userAccount.labo
-        );
+            counter++;
+        }
+        return finalePages;
     }
 }
