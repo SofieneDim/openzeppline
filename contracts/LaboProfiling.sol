027b1ee3138d23b7e8712794200015a226aec0f4
sofienedim <dimessisofiene@gmail.com>
2020-10-13 12:00:25 +0100

    auto push

--- contracts/LaboProfiling.sol
+++ contracts/LaboProfiling.sol
@@ -2,9 +2,9 @@ pragma experimental ABIEncoderV2;
 pragma solidity >0.4.99 <0.6.0;
 
 contract LaboProfiling {
-    struct adminAccount {
+    struct laboProfile {
         uint256 id;
-        uint256 machinesId;
+        uint256[] machinesIds;
         string gestionType;
     }
 
@@ -22,6 +22,25 @@ contract LaboProfiling {
     uint256 machinesCounter;
     uint256 humanResourceCounter;
 
-    mapping(uint256 => machine) public machines;
-    mapping(uint256 => humanResource) public humansResources;
+    mapping(uint256 => machine) private machines;
+    mapping(uint256 => laboProfile) private labosProfiles;
+    mapping(uint256 => humanResource) private humansResources;
+
+    function addMachine(
+        uint256 laboId,
+        string memory serialNumber,
+        string memory brand
+    ) public {
+        machinesCounter++;
+        machines[machinesCounter] = machine(serialNumber, brand);
+        labosProfiles[laboId].machinesIds.push(machinesCounter);
+    }
+
+    function getProfile(uint256 laboId)
+        public
+        view
+        returns (laboProfile memory)
+    {
+        return labosProfiles[laboId];
+    }
 }
